3613f2c949a031739052e318a64fc4f13a095ab5
DunJohnathon <johndavidwaskow@gmail.com>
2026-05-24 10:23:03 -0500

    Update .eleventy.js
    passthrough assets

--- .eleventy.js
+++ .eleventy.js
@@ -715,6 +715,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
